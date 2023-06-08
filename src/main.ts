6a9fa563740358eba52c32baa86b13466ba852c4
ZengDa0501 <smirnovvvasyl@gmail.com>
2023-06-08 07:02:12 +0000

    feat: latest

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,25 @@
-import express from "express";
 import cors from "cors";
+import express from "express";
+import passport from "passport"
 import mongoose from "mongoose";
+import bodyParser from "body-parser"
 import rateLimit from 'express-rate-limit'
+import fileUpload from 'express-fileupload';
 
 // External Modules
+// import './subscribe/events'
 import { Routes } from "./Routes";
-import { config } from "./config/config";
-import setlog from "./utils/setlog";
+import { setlog } from "./utils/setlog";
+import { config } from './config/config';
+// import { initHandler } from "./blockchain/api";
+// import { setupSocketServer } from "./socketServer";
 
 // Get router
 const router: express.Router = express.Router();
 const app: express.Express = express();
 const limiter = rateLimit({
 	windowMs: 15 * 60 * 1000, // 15 minutes
-	max: 200, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+	max: 20000, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
 	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
 	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
 })
@@ -35,11 +41,21 @@ const connectDatabase = async (mongoUrl: string) => {
 	}
 };
 
+// Body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(fileUpload({
+	limits: { fileSize: 50 * 1024 * 1024 }
+}));
+
+// Passport middleware
+app.use(passport.initialize());
+
 // Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({ origin: "*", methods: ["POST", "GET"] }));
-app.use(limiter)
 app.use(express.json());
+app.use(limiter)
 
 // Frontend Render
 if (!config.DEBUG) {
@@ -57,11 +73,16 @@ if (!config.DEBUG) {
 Routes(router);
 app.use("/api", router);
 
-
 connectDatabase(config.DATABASE).then(() => {
-	app.listen(config.PORT, () => {
+	// // run blockchain handler
+	// initHandler();
+
+	const server = app.listen(config.PORT, () => {
 		setlog(`Server listening on ${config.PORT} port`);
 	});
+
+	// // run socket server
+	// setupSocketServer(server)
 }).catch((err: any) => {
 	setlog(err);
 });
